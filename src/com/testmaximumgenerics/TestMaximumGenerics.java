d79bf1eb6ba19de32ff863c5ba40356bb8ff05ab
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-13 09:42:42 +0530

    ADDED: Test Maximum Generics Program with welcome message

---
+++ src/com/testmaximumgenerics/TestMaximumGenerics.java
@@ -0,0 +1,7 @@
+package com.testmaximumgenerics;
+
+public class TestMaximumGenerics {
+	public static void main(String[] args) {
+		System.out.println("Welcome to test maximum generics program");
+	}
+}
