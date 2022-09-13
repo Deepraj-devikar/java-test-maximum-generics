10a795267c61e6e5485fabeb2f833477c3d41e58
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-13 10:37:07 +0530

    ADDED: test case 1.2 maximum integer at second postion

--- src/com/testmaximumgenerics/TestMaximumGenerics.java
+++ src/com/testmaximumgenerics/TestMaximumGenerics.java
@@ -14,9 +14,16 @@ public class TestMaximumGenerics {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to test maximum generics program");
+		System.out.println("Test case 1.1");
 		// Test Case 1.1 maximum number at first position 
 		System.out.println("Maximum of 3, 2, 1 is : "+TestMaximumGenerics.maximumInteger(3, 2, 1));
 		System.out.println("Maximum of 3, 1, 2 is : "+TestMaximumGenerics.maximumInteger(3, 1, 2));
 		System.out.println("Maximum of 3, 1, 1 is : "+TestMaximumGenerics.maximumInteger(3, 1, 1));
+		
+		System.out.println("Test case 1.2");
+		// Test Case 1.1 maximum number at first position 
+		System.out.println("Maximum of 2, 3, 1 is : "+TestMaximumGenerics.maximumInteger(2, 3, 1));
+		System.out.println("Maximum of 1, 3, 2 is : "+TestMaximumGenerics.maximumInteger(1, 3, 2));
+		System.out.println("Maximum of 1, 3, 1 is : "+TestMaximumGenerics.maximumInteger(1, 3, 1));
 	}
 }
