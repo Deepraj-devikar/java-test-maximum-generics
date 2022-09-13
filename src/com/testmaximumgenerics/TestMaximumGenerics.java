12acf91f726843d029b887cf63384596d4fafd40
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-13 10:29:24 +0530

    ADDED: find maximum integer in three integers

--- src/com/testmaximumgenerics/TestMaximumGenerics.java
+++ src/com/testmaximumgenerics/TestMaximumGenerics.java
@@ -1,7 +1,19 @@
 package com.testmaximumgenerics;
 
 public class TestMaximumGenerics {
+
+	public static Integer maximumInteger(Integer first, Integer second, Integer third) {
+		if (first > second && first > third) {
+			return first;
+		} else if (second > third) {
+			return second;
+		} else {
+			return third;
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to test maximum generics program");
+		System.out.println("Maximum of 3, 2, 1 is : "+TestMaximumGenerics.maximumInteger(3, 2, 1));
 	}
 }
